4ffc48af123405bc646edc37153d585b5d56500b
gamecore08 <prasetyohandydrops@gmail.com>
2026-01-16 01:01:33 +0700

    fix: use client must be first

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
+"use client";
+
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
-
-"use client";
 
 import React, { useEffect, useMemo, useRef, useState } from "react";
 import { useSearchParams } from "next/navigation";
